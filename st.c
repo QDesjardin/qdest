73b605fb223e4b37e8d4aba1d418fcb76748836c
Daniel Craig <dairy@queendairy.com>
2026-07-25 11:27:59 -0600

    asdasd

--- st.c
+++ st.c
@@ -817,22 +817,24 @@ execsh(char *cmd, char **args)
 void
 sigchld(int a)
 {
-	int stat;
+	int stat, olderrno;
 	pid_t p;
 
-	while ((p = waitpid(-1, &stat, WNOHANG)) > 0) {
+	olderrno = errno;
+
+	while ((p = waitpid(-1, &stat, WNOHANG)) > 0 || (p < 0 && errno == EINTR)) {
 		if (p == pid) {
 			#if EXTERNALPIPEIN_PATCH && EXTERNALPIPE_PATCH
 			close(csdfd);
 			#endif // EXTERNALPIPEIN_PATCH
 
-			if (WIFEXITED(stat) && WEXITSTATUS(stat))
-				die("child exited with status %d\n", WEXITSTATUS(stat));
-			else if (WIFSIGNALED(stat))
-				die("child terminated due to signal %d\n", WTERMSIG(stat));
+			if ((WIFEXITED(stat) && WEXITSTATUS(stat)) || WIFSIGNALED(stat))
+				_exit(1);
 			_exit(0);
 		}
 	}
+
+	errno = olderrno;
 }
 
 void
@@ -913,6 +915,8 @@ ttynew(const char *line, char *cmd, const char *out, char **args)
 #ifdef __OpenBSD__
 		#if RIGHTCLICKTOPLUMB_PATCH || OPENCOPIED_PATCH
 		if (pledge("stdio rpath tty proc ps exec", NULL) == -1)
+		#elif EXTERNALPIPE_PATCH
+		if (pledge("stdio rpath tty proc exec", NULL) == -1)
 		#else
 		if (pledge("stdio rpath tty proc", NULL) == -1)
 		#endif // RIGHTCLICKTOPLUMB_PATCH
@@ -1101,6 +1105,9 @@ void
 tsetdirt(int top, int bot)
 {
 	int i;
+
+	if (term.row <= 0)
+		return;
 
 	LIMIT(top, 0, term.row-1);
 	LIMIT(bot, 0, term.row-1);
@@ -3332,6 +3339,7 @@ eschandle(uchar ascii)
 		resettitle();
 		xloadcols();
 		xsetmode(0, MODE_HIDE);
+		xsetmode(0, MODE_BRCKTPASTE);
 		#if SCROLLBACK_PATCH && !REFLOW_PATCH
 		if (!IS_SET(MODE_ALTSCREEN)) {
 			term.scr = 0;
@@ -3466,6 +3474,11 @@ check_control_code:
 			return;
 		#if SIXEL_PATCH
 		} else if (term.esc & ESC_DCS) {
+			/* Skip if DCS escape sequence buffer is full */
+			if (csiescseq.len >= sizeof(csiescseq.buf) - 1) {
+				return;
+			}
+
 			csiescseq.buf[csiescseq.len++] = u;
 			if (BETWEEN(u, 0x40, 0x7E)
 					|| csiescseq.len >= \
